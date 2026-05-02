9c6ccb61d023cf404e692804057c0f16c099e18d
noumankhan95 <noumankhan95@yahoo.com>
2026-05-02 07:22:24 +0400

    adding custom error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,13 +5,18 @@ import { SigninRouter } from "./routes/signin";
 import { SignoutRouter } from "./routes/signout";
 import { CurrentUserRouter } from "./routes/current-user";
 import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found";
 const app = express();
 app.use(json());
 app.use(SigninRouter)
 app.use(SignupRouter)
 app.use(SignoutRouter)
 app.use(CurrentUserRouter)
+app.get("*",()=>{
+    throw new NotFoundError();
+})
 app.use(errorHandler)
+
 app.listen(3000, () => {
     console.log("Listening on 3000")
 })
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import { body, validationResult } from "express-validator"
+import { RequestValidationError } from "../errors/request-validation-error";
 const router = Router();
 
 router.post("/api/users/signup", [body("email").isEmail().withMessage("Email must be valid"), body("password").trim().isLength({ min: 5, max: 20 })], (req: Request, res: Response) => {
@@ -7,7 +8,7 @@ router.post("/api/users/signup", [body("email").isEmail().withMessage("Email mus
     const password = req.body.password;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).send(errors.array())
+        throw new RequestValidationError(errors.array())
     }
 
     res.send("User created")

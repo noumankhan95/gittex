6524222383012c2df4fd6dcccf464bd6de44ee03
noumankhan95 <noumankhan95@yahoo.com>
2026-05-04 04:12:36 +0400

    adding cokkie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,17 +6,32 @@ import { SignoutRouter } from "./routes/signout";
 import { CurrentUserRouter } from "./routes/current-user";
 import { errorHandler } from "./middleware/error-handler";
 import { NotFoundError } from "./errors/not-found";
+import mongoose from "mongoose"
+import cookieSession from "cookie-session";
 const app = express();
+app.set("proxy", true)
+app.use(cookieSession({
+    secure: true,
+
+}))
 app.use(json());
 app.use(SigninRouter)
 app.use(SignupRouter)
 app.use(SignoutRouter)
 app.use(CurrentUserRouter)
-app.get("*",()=>{
+app.get("*", () => {
     throw new NotFoundError();
 })
 app.use(errorHandler)
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth")
+    } catch (e) {
+        console.log(e)
+    }
+    app.listen(3000, () => {
+        console.log("Listening on 3000")
+    })
+}
 
-app.listen(3000, () => {
-    console.log("Listening on 3000")
-})
+start()
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,16 +1,23 @@
 import { Router, Request, Response } from "express";
 import { body, validationResult } from "express-validator"
 import { RequestValidationError } from "../errors/request-validation-error";
+import { User } from "../models/user-schema";
+import { BadRequestError } from "../errors/bad-request-error";
 const router = Router();
 
-router.post("/api/users/signup", [body("email").isEmail().withMessage("Email must be valid"), body("password").trim().isLength({ min: 5, max: 20 })], (req: Request, res: Response) => {
+router.post("/api/users/signup", [body("email").isEmail().withMessage("Email must be valid"), body("password").trim().isLength({ min: 5, max: 20 })], async (req: Request, res: Response) => {
     const email = req.body.email;
     const password = req.body.password;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         throw new RequestValidationError(errors.array())
     }
-
-    res.send("User created")
+    const exisitingUser = await User.findOne({ email })
+    if (exisitingUser) {
+        throw new BadRequestError("Email Already in Use")
+    }
+    const user = User.build({ email, password })
+    await user.save();
+    res.status(201).send(user)
 })
 export { router as SignupRouter };

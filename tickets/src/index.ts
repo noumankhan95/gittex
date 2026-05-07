7f171ecbe5fe3a7b2affca54a45e85361c23be13
noumankhan95 <noumankhan95@yahoo.com>
2026-05-07 07:40:39 +0400

    adding env variable in depl

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,9 +4,12 @@ const start = async () => {
     if (!process.env.JWT_KEY) {
         throw new Error("JWT Must be defined")
     }
+    if (!process.env.MONGO_URI) {
+        throw new Error("Mongo URI must be defined")
+    }
     try {
-        await mongoose.connect("mongodb://auth-mongo-srv:27017/tickets")
-    } catch (e) {
+        await mongoose.connect(process.env.MONGO_URI)
+    } catch (e) { 
         console.log(e)
     }
     app.listen(3000, () => {
